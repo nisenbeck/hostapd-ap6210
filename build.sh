fcd520e7ce298b0940a31846c148132c95898878
Nico Isenbeck <nico.isenbeck@web.de>
2015-08-27 12:30:07 +0000

    rename to package to hostapd-ap6210

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "Please ensure you install the toolchain and build dependencies first!"
-make clean && cd .. && tar -cvzf hostapd-rtl_2.4.orig.tar.gz hostapd-rtl && cd hostapd-rtl
+make clean && cd .. && tar -cvzf hostapd-ap6210_2.4.orig.tar.gz hostapd-ap6210 && cd hostapd-ap6210
 debuild -us -uc
